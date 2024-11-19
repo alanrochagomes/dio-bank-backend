5708bcd61e6922747f9cb81880524764b9ae05e0
Alan Rocha <alanrochaarg2001@gmail.com>
2024-11-19 13:55:22 -0300

    refactor(package): atualizando versão de `@types/express`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import express, { Request, Response } from "express";
+
+const server = express();
+
+server.get("/", (request: Request, response: Response) => {
+  return response.status(200).json({ message: "DioBank Api" });
+});
+
+server.listen(5000, () => console.log("Server on"));
