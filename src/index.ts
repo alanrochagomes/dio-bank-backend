ab2b18fc42a8991d08b686a25ac6a6095d82d005
Alan Rocha <alanrochaarg2001@gmail.com>
2024-11-19 15:34:47 -0300

    feat: isolando as rotas

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import express, { Request, Response } from "express";
-import { UserController } from "./controllers/UserController";
-
-const userController = new UserController();
+import { router } from "./routes";
 
 const server = express();
 
 server.use(express.json());
+server.use(router);
 
 server.get("/", (request: Request, response: Response) => {
   return response.status(200).json({ message: "DioBank Api" });
 });
-
-server.post("/user", userController.createUser);
-server.get("/user", userController.getAllUsers);
 
 server.listen(5000, () => console.log("Server on"));
