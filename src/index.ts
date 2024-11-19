8ed3d167a9f3d2a953c10f725f9e707734e2b20a
Alan Rocha <alanrochaarg2001@gmail.com>
2024-11-19 14:09:39 -0300

    feat(src/index.ts): criando rota

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import express, { Request, Response } from "express";
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (request: Request, response: Response) => {
   return response.status(200).json({ message: "DioBank Api" });
+});
+
+server.post("/user", (request: Request, response: Response) => {
+  const body = request.body;
+  console.log(body);
+  return response.status(201).json({ message: "Usuario criado" });
 });
 
 server.listen(5000, () => console.log("Server on"));
